0b1943d56fa6f0acee3daa97d997b771c9104fc8
Lucas Davila <nightdavila@gmail.com>
2023-10-30 17:55:40 -0300

    Revert "Feature/permissoes de users  inssue model"

--- backend/prisma/migrations/20231026025701_inssue_information_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `information` to the `inssues` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "inssues" ADD COLUMN     "information" TEXT NOT NULL;
